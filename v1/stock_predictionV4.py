0cde670725ce09dd19cb5fe539dcf10b0f5d1ac3
AmandaKingdom <105119862@student.swin.edu.au>
2025-09-15 18:04:02 +1000

    v4

--- v1/stock_predictionV4.py
+++ v1/stock_predictionV4.py
@@ -152,46 +152,16 @@ PRICE_VALUE = "Close"
 data_dict = load_data(COMPANY, TRAIN_START, TRAIN_END)
 data = data_dict["df"]  # the original preprocessed DataFrame
 
-scaler = MinMaxScaler(feature_range=(0, 1)) 
+scaler = data_dict["scalers"][PRICE_VALUE] 
 # Note that, by default, feature_range=(0, 1). Thus, if you want a different 
 # feature_range (min,max) then you'll need to specify it here
-scaled_data = scaler.fit_transform(data[PRICE_VALUE].values.reshape(-1, 1)) 
-# Flatten and normalise the data
-# First, we reshape a 1D array(n) to 2D array(n,1)
-# We have to do that because sklearn.preprocessing.fit_transform()
-# requires a 2D array
-# Here n == len(scaled_data)
-# Then, we scale the whole array to the range (0,1)
-# The parameter -1 allows (np.)reshape to figure out the array size n automatically 
-# values.reshape(-1, 1) 
-# https://stackoverflow.com/questions/18691084/what-does-1-mean-in-numpy-reshape'
-# When reshaping an array, the new shape must contain the same number of elements 
-# as the old shape, meaning the products of the two shapes' dimensions must be equal. 
-# When using a -1, the dimension corresponding to the -1 will be the product of 
-# the dimensions of the original array divided by the product of the dimensions 
-# given to reshape so as to maintain the same number of elements.
-
 # Number of days to look back to base the prediction
-PREDICTION_DAYS = 60 # Original
+
 
 # To store the training data
-x_train = []
-y_train = []
-
-scaled_data = scaled_data[:,0] # Turn the 2D array back to a 1D array
-# Prepare the data
-for x in range(PREDICTION_DAYS, len(scaled_data)):
-    x_train.append(scaled_data[x-PREDICTION_DAYS:x])
-    y_train.append(scaled_data[x])
-
-# Convert them into an array
-x_train, y_train = np.array(x_train), np.array(y_train)
-# Now, x_train is a 2D array(p,q) where p = len(scaled_data) - PREDICTION_DAYS
-# and q = PREDICTION_DAYS; while y_train is a 1D array(p)
-
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-# We now reshape x_train into a 3D array(p, q, 1); Note that x_train 
-# is an array of p inputs with each input being a 2D array 
+x_train = data_dict["X_train"]
+y_train = data_dict["y_train"]
+# Extract preprocessed training data (already 3D for LSTM)
 
 #------------------------------------------------------------------------------
 # Build the Model
@@ -205,7 +175,8 @@ model = Sequential() # Basic neural network
 # See: https://www.tensorflow.org/api_docs/python/tf/keras/Sequential
 # for some useful examples
 
-model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], x_train.shape[2])))
+
 # This is our first hidden layer which also spcifies an input layer. 
 # That's why we specify the input shape for this layer; 
 # i.e. the format of each training example
@@ -276,54 +247,10 @@ model.fit(x_train, y_train, epochs=10, batch_size=32)
 #------------------------------------------------------------------------------
 # Test the model accuracy on existing data
 #------------------------------------------------------------------------------
-# Load the test data
-TEST_START = '2023-08-02'
-TEST_END = '2024-07-02'
+x_test = data_dict["X_test"]
+y_test = data_dict["y_test"]
 
-# test_data = web.DataReader(COMPANY, DATA_SOURCE, TEST_START, TEST_END)
-
-test_data = yf.download(COMPANY,TEST_START,TEST_END)
-
-# The above bug is the reason for the following line of code
-# test_data = test_data[1:]
-
-actual_prices = test_data[PRICE_VALUE].values
-
-total_dataset = pd.concat((data[PRICE_VALUE], test_data[PRICE_VALUE]), axis=0)
-
-
-model_inputs = total_dataset[len(total_dataset) - len(test_data) - PREDICTION_DAYS:].values
-# We need to do the above because to predict the closing price of the fisrt
-# PREDICTION_DAYS of the test period [TEST_START, TEST_END], we'll need the 
-# data from the training period
-
-model_inputs = model_inputs.reshape(-1, 1)
-# TO DO: Explain the above line
-
-model_inputs = scaler.transform(model_inputs)
-# We again normalize our closing price data to fit them into the range (0,1)
-# using the same scaler used above 
-# However, there may be a problem: scaler was computed on the basis of
-# the Max/Min of the stock price for the period [TRAIN_START, TRAIN_END],
-# but there may be a lower/higher price during the test period 
-# [TEST_START, TEST_END]. That can lead to out-of-bound values (negative and
-# greater than one)
-# We'll call this ISSUE #2
-
-# TO DO: Generally, there is a better way to process the data so that we 
-# can use part of it for training and the rest for testing. You need to 
-# implement such a way
-
-#------------------------------------------------------------------------------
-# Make predictions on test data
-#------------------------------------------------------------------------------
-x_test = []
-for x in range(PREDICTION_DAYS, len(model_inputs)):
-    x_test.append(model_inputs[x - PREDICTION_DAYS:x, 0])
-
-x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-# TO DO: Explain the above 5 lines
+actual_prices = scaler.inverse_transform(y_test.reshape(-1, 1))
 
 predicted_prices = model.predict(x_test)
 predicted_prices = scaler.inverse_transform(predicted_prices)
@@ -386,10 +313,10 @@ plot_boxplot(data, n_days = 700)
 #------------------------------------------------------------------------------
 # Predict next day
 #------------------------------------------------------------------------------
-real_data = [model_inputs[len(model_inputs) - PREDICTION_DAYS:, 0]]
-real_data = np.array(real_data)
-real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+real_data = data_dict["last_sequence"]
+real_data = np.expand_dims(real_data, axis=0)
 prediction = model.predict(real_data)
+
 prediction = scaler.inverse_transform(prediction)
 print(f"Prediction: {prediction}")
 
